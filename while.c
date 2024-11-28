c4608cc2d300045e8d74d87a5a628462a1b5451b
Aditya2006452 <adityanadiger70@gmail.com>
2024-11-28 23:13:35 +0530

    Update while loop.c

--- while.c
+++ while.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
-int main()
-{
-int i=1;
-while(i<=7)
-{
-printf("%d",i);
-i++;
-return 0;
-}
+int main() {
+    int i = 1;
+    while (i <= 7) {
+        printf("%d", i);
+        i++;
+    }
+    return 0;
 }
